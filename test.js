a4bc5493b57351b54a7d0dea1bae7b89a7e36b23
salvatoreliaoxuan <salvatoreliaoxuan@didiglobal.com>
2021-08-20 11:57:01 +0800

    perf: test

--- test.js
+++
@@ -1,5 +0,0 @@
-const download = require('download-git-repo')
-const url = 'https://git.xiaojukeji.com:taxi-fe/mp-kuaidi#master'
-download(url,'./', { clone: true }, (err) => {
-    console.log('=====>',err)
-})
