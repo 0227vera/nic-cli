24798c94fe0fdc4ab901abbec7a97950abf07b4c
salvatoreliaoxuan <salvatoreliaoxuan@didiglobal.com>
2021-08-26 20:51:12 +0800

    feat: 微前端项目

--- src/help.js
+++ src/help.js
@@ -3,6 +3,7 @@ module.exports = async function (params) {
     '\n nic init [type]\n' +
     '\n h5项目: nic init h5 \n' +
     '\n web项目: nic init web \n' +
+    '\n 微前端项目: nic init micro \n' +
     '\n node项目: nic init node \n' +
     '\n 小程序: nic init mp \n'
   )
